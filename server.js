1aa403384054841fe3d4925da05db9f3a83127f0
ilci66 <akbiyik.ilker7@gmail.com>
2022-01-13 11:34:11 +0100

    working on babel & backend

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser')
 const routes = require('./routes/index.js')
-
+const helmet = require('helmet')
 const app = express();
 
 app.use(helmet());
